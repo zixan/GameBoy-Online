8d9e08ffedbcb5270a4c00fa81e3b1638b9c32dc
Grant Galitz <grantgalitz@gmail.com>
2011-03-11 20:21:20 -0500

    Adding a check to prevent a javascript error from occurring.

--- js/GameBoyIO.js
+++ js/GameBoyIO.js
@@ -126,14 +126,19 @@ function saveSRAM() {
 	}
 }
 function saveRTC() {	//Execute this when SRAM is being saved as well.
-	if (gameboy.cTIMER) {
-		try {
-			cout("Saving the RTC...", 0);
-			setValue("RTC_" + gameboy.name, gameboy.saveRTCState());
+	if (typeof gameboy == "object" && gameboy != null) {
+		if (gameboy.cTIMER) {
+			try {
+				cout("Saving the RTC...", 0);
+				setValue("RTC_" + gameboy.name, gameboy.saveRTCState());
+			}
+			catch (error) {
+				cout("Could not save the RTC of the current emulation state(\"" + error.message + "\").", 2);
+			}
 		}
-		catch (error) {
-			cout("Could not save the RTC of the current emulation state(\"" + error.message + "\").", 2);
-		}
+	}
+	else {
+		cout("GameBoy core cannot be saved while it has not been initialized.", 1);
 	}
 }
 function openSRAM(filename) {
