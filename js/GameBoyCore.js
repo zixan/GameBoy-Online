62f7dd2cd0b7c24150c609fe7b8c536274ee5a49
Grant Galitz <grantgalitz@gmail.com>
2011-06-21 20:06:11 -0400

    Updated Updating the midscanline working pixel determinism.

--- js/GameBoyCore.js
+++ js/GameBoyCore.js
@@ -5852,7 +5852,7 @@ GameBoyCore.prototype.renderScanLine = function () {
 }
 GameBoyCore.prototype.renderMidScanLine = function () {
 	if (this.actualScanLine < 144 && this.modeSTAT == 3 && (settings[4] == 0 || this.frameCount > 0)) {
-		var pixelEnd = Math.floor(160 * Math.min(Math.max(this.LCDTicks - 23, 0) / 40, 1));
+		var pixelEnd = Math.min(Math.floor(160 * Math.min(Math.max(this.LCDTicks - 23, 0) / 40, 1)) + 4, 160);
 		if (this.bgEnabled) {
 			this.pixelStart = this.actualScanLine * 160;
 			this.BGLayerRender(pixelEnd);
