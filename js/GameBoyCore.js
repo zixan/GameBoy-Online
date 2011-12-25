6345db640eb65917e94b17fbe07252eca6270b55
Grant Galitz <grantgalitz@gmail.com>
2011-12-24 23:44:22 -0500

    Removing a stray semicolon.

--- js/GameBoyCore.js
+++ js/GameBoyCore.js
@@ -717,7 +717,7 @@ GameBoyCore.prototype.OPCODE = [
 	},
 	//ADD HL, HL
 	//#0x29:
-	function (parentObj) {;
+	function (parentObj) {
 		parentObj.FHalfCarry = ((parentObj.registersHL & 0xFFF) > 0x7FF);
 		parentObj.FCarry = (parentObj.registersHL > 0x7FFF);
 		parentObj.registersHL = (parentObj.registersHL << 1) & 0xFFFF;
