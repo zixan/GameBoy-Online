6484075678cb381aaa760d0a6a0a7fc99fdc4a93
Grant Galitz <grantgalitz@gmail.com>
2012-02-08 07:11:31 -0500

    Software gfx resizer update.

--- js/other/resize.js
+++ js/other/resize.js
@@ -39,14 +39,11 @@ Resize.prototype.initialize = function () {
 	}
 }
 Resize.prototype.compileWidthResize = function () {
-	this.ratioWidthWeight = this.widthOriginal / this.targetWidth;
 	var widthOrigMulChannels = this.widthOriginal << 2;
 	var heightOrigMulChannels = this.heightOriginal << 2;
 	var widthTargetMulChannels = this.targetWidth << 2;
-	var pixelOffset = 0;
-	var temp_var = 0;
 	var toCompile = "\
-	var ratioWeight = this.ratioWidthWeight;\
+	var ratioWeight = " + (this.widthOriginal / this.targetWidth) + ";\
 	var weight = 0;\
 	var actualPosition = 0;\
 	var amountToNext = 0;\
@@ -54,110 +51,85 @@ Resize.prototype.compileWidthResize = function () {
 	var outputOffset = 0;\
 	var currentPosition = 0;\
 	var output = this.outputWidthWorkBench;\
+	var line = 0;\
+	var pixelOffset = 0;\
 	do {\
-		weight = ratioWeight;";
-	for (line = 0; line < heightOrigMulChannels; ++line) {
-		toCompile += "\
-		output[" + line + "] = 0;";
-	}
-	toCompile += "\
+		weight = ratioWeight;\
+		for (line = 0; line < " + heightOrigMulChannels + "; ++line) {\
+			output[line] = 0;\
+		}\
 		while (weight > 0 && actualPosition < " + widthOrigMulChannels + ") {\
 			amountToNext = 1 + actualPosition - currentPosition;\
-			if (weight >= amountToNext) {";
-	for (pixelOffset = line = 0; line < heightOrigMulChannels; pixelOffset += widthOrigMulChannels) {
-		temp_var = pixelOffset;
-		toCompile += "\
-				output[" + (line++) + "] += buffer[actualPosition" + ((temp_var > 0) ? (" + " + temp_var) : "") + "] * amountToNext;";
-		++temp_var;		
-		toCompile += "\
-				output[" + (line++) + "] += buffer[actualPosition + " + (temp_var++) + "] * amountToNext;\
-				output[" + (line++) + "] += buffer[actualPosition + " + (temp_var++) + "] * amountToNext;\
-				output[" + (line++) + "] += buffer[actualPosition + " + temp_var + "] * amountToNext;";
-	}
-	toCompile += "\
+			if (weight >= amountToNext) {\
+				for (pixelOffset = line = 0; line < " + heightOrigMulChannels + "; pixelOffset += " + widthOrigMulChannels + ") {\
+					output[line++] += buffer[actualPosition + pixelOffset] * amountToNext;\
+					output[line++] += buffer[actualPosition + pixelOffset + 1] * amountToNext;\
+					output[line++] += buffer[actualPosition + pixelOffset + 2] * amountToNext;\
+					output[line++] += buffer[actualPosition + pixelOffset + 3] * amountToNext;\
+				}\
 				currentPosition = actualPosition = actualPosition + 4;\
 				weight -= amountToNext;\
 			}\
-			else {";
-	for (pixelOffset = line = 0; line < heightOrigMulChannels; pixelOffset += widthOrigMulChannels) {
-		temp_var = pixelOffset;
-		toCompile += "\
-				output[" + (line++) + "] += buffer[actualPosition" + ((temp_var > 0) ? (" + " + temp_var) : "") + "] * weight;";
-		++temp_var;		
-		toCompile += "\
-				output[" + (line++) + "] += buffer[actualPosition + " + (temp_var++) + "] * weight;\
-				output[" + (line++) + "] += buffer[actualPosition + " + (temp_var++) + "] * weight;\
-				output[" + (line++) + "] += buffer[actualPosition + " + temp_var + "] * weight;";
-	}
-	toCompile += "\
+			else {\
+				for (pixelOffset = line = 0; line < " + heightOrigMulChannels + "; pixelOffset += " + widthOrigMulChannels + ") {\
+					output[line++] += buffer[actualPosition + pixelOffset] * weight;\
+					output[line++] += buffer[actualPosition + pixelOffset + 1] * weight;\
+					output[line++] += buffer[actualPosition + pixelOffset + 2] * weight;\
+					output[line++] += buffer[actualPosition + pixelOffset + 3] * weight;\
+				}\
 				currentPosition += weight;\
 				break;\
 			}\
-		}";
-	for (pixelOffset = line = 0; line < heightOrigMulChannels; pixelOffset += widthTargetMulChannels) {
-		temp_var = pixelOffset;
-		toCompile += "\
-		outputBuffer[outputOffset" + ((temp_var > 0) ? (" + " + temp_var) : "") + "] = output[" + (line++) + "] / ratioWeight;";
-		++temp_var;
-		toCompile += "\
-		outputBuffer[outputOffset + " + (temp_var++) + "] = output[" + (line++) + "] / ratioWeight;\
-		outputBuffer[outputOffset + " + (temp_var++) + "] = output[" + (line++) + "] / ratioWeight;\
-		outputBuffer[outputOffset + " + temp_var + "] = output[" + (line++) + "] / ratioWeight;";
-	}
-	toCompile += "\
+		}\
+		for (pixelOffset = line = 0; line < " + heightOrigMulChannels + "; pixelOffset += " + widthTargetMulChannels + ") {\
+			outputBuffer[outputOffset + pixelOffset] = output[line++] / ratioWeight;\
+			outputBuffer[outputOffset + pixelOffset + 1] = output[line++] / ratioWeight;\
+			outputBuffer[outputOffset + pixelOffset + 2] = output[line++] / ratioWeight;\
+			outputBuffer[outputOffset + pixelOffset + 3] = output[line++] / ratioWeight;\
+		}\
 		outputOffset += 4;\
 	} while (outputOffset < " + widthTargetMulChannels + ");\
 	return outputBuffer;";
 	this.resizeWidth = Function("buffer", toCompile);
 }
 Resize.prototype.compileHeightResize = function () {
-	this.ratioHeightWeight = this.heightOriginal / this.targetHeight;
-	var totalChannels = this.targetWidth * this.targetHeight * 4;
-	var totalPrevChannels = this.targetWidth * this.heightOriginal * 4;
 	var targetWidthMul = this.targetWidth << 2;
 	var toCompile = "\
-	var ratioWeight = this.ratioHeightWeight;\
+	var ratioWeight = " + (this.heightOriginal / this.targetHeight) + ";\
 	var weight = 0;\
 	var actualPosition = 0;\
 	var amountToNext = 0;\
-	var outputBuffer = this.heightBuffer;\
 	var outputOffset = 0;\
 	var currentPosition = 0;\
+	var pixelOffset = 0;\
 	var output = this.outputHeightWorkBench;\
+	var outputBuffer = this.heightBuffer;\
 	do {\
-		weight = ratioWeight;";
-	for (pixelOffset = 0; pixelOffset < targetWidthMul; ++pixelOffset) {
-		toCompile += "\
-		output[" + pixelOffset + "] = 0;";
-	}
-	toCompile += "\
-		while (weight > 0 && actualPosition < " + totalPrevChannels + ") {\
+		weight = ratioWeight;\
+		for (pixelOffset = 0; pixelOffset < " + targetWidthMul + "; ++pixelOffset) {\
+			output[pixelOffset] = 0;\
+		}\
+		while (weight > 0 && actualPosition < " + (targetWidthMul * this.heightOriginal) + ") {\
 			amountToNext = 1 + actualPosition - currentPosition;\
-			if (weight >= amountToNext) {";
-	for (pixelOffset = 0; pixelOffset < targetWidthMul; ++pixelOffset) {
-		toCompile += "\
-				output[" + pixelOffset + "] += buffer[actualPosition++] * amountToNext;";
-	}
-	toCompile += "\
+			if (weight >= amountToNext) {\
+				for (pixelOffset = 0; pixelOffset < " + targetWidthMul + "; ++pixelOffset) {\
+					output[pixelOffset] += buffer[actualPosition++] * amountToNext;\
+				}\
 				currentPosition = actualPosition;\
 				weight -= amountToNext;\
 			}\
-			else {";
-	for (pixelOffset = 0; pixelOffset < targetWidthMul; ++pixelOffset) {
-		toCompile += "\
-				output[" + pixelOffset + "] += buffer[actualPosition" + ((pixelOffset > 0) ? (" + " + pixelOffset) : "") + "] * weight;";
-	}
-	toCompile += "\
+			else {\
+				for (pixelOffset = 0; pixelOffset < " + targetWidthMul + "; ++pixelOffset) {\
+					output[pixelOffset] += buffer[actualPosition + pixelOffset] * weight;\
+				}\
 				currentPosition += weight;\
 				break;\
 			}\
-		}";
-	for (pixelOffset = 0; pixelOffset < targetWidthMul; ++pixelOffset) {
-		toCompile += "\
-		outputBuffer[outputOffset++] = output[" + pixelOffset + "] / ratioWeight;";
-	}
-	toCompile += "\
-	} while (outputOffset < " + totalChannels + ");\
+		}\
+		for (pixelOffset = 0; pixelOffset < " + targetWidthMul + "; ++pixelOffset) {\
+			outputBuffer[outputOffset++] = output[pixelOffset] / ratioWeight;\
+		}\
+	} while (outputOffset < " + (targetWidthMul * this.targetHeight) + ");\
 	return outputBuffer;";
 	this.resizeHeight = Function("buffer", toCompile);
 }
