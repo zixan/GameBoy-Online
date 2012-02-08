31537beb44c5a5654744bdbb4341c3f9b848a108
Grant Galitz <grantgalitz@gmail.com>
2012-02-08 07:46:54 -0500

    Software gfx resizer update.

--- js/other/resize.js
+++ js/other/resize.js
@@ -4,6 +4,11 @@ function Resize(widthOriginal, heightOriginal, targetWidth, targetHeight) {
 	this.heightOriginal = heightOriginal >>> 0;
 	this.targetWidth = targetWidth >>> 0;
 	this.targetHeight = targetHeight >>> 0;
+	this.targetWidthMultipliedByChannels = this.targetWidth << 2;
+	this.originalWidthMultipliedByChannels = this.widthOriginal << 2;
+	this.originalHeightMultipliedByChannels = this.heightOriginal << 2;
+	this.widthPassResultSize = this.targetWidthMultipliedByChannels * this.heightOriginal;
+	this.finalResultSize = this.targetWidthMultipliedByChannels * this.targetHeight;
 	this.initialize();
 }
 Resize.prototype.initialize = function () {
@@ -15,22 +20,22 @@ Resize.prototype.initialize = function () {
 		}
 		else if (this.widthOriginal == this.targetWidth) {
 			//Bypass the width resizer pass:
+			this.ratioWeightHeightPass = this.heightOriginal / this.targetHeight;
 			this.initializeSecondPassBuffers();
-			this.compileHeightResize();
 			this.resize = this.resizeHeight;
 		}
 		else if (this.heightOriginal == this.targetHeight) {
 			//Bypass the height resizer pass:
+			this.ratioWeightWidthPass = this.widthOriginal / this.targetWidth;
 			this.initializeFirstPassBuffers();
-			this.compileWidthResize();
 			this.resize = this.resizeWidth;
 		}
 		else {
 			//Resize the image with two passes:
+			this.ratioWeightWidthPass = this.widthOriginal / this.targetWidth;
+			this.ratioWeightHeightPass = this.heightOriginal / this.targetHeight;
 			this.initializeFirstPassBuffers();
 			this.initializeSecondPassBuffers();
-			this.compileWidthResize();
-			this.compileHeightResize();
 			this.resize = this.resizer;
 		}
 	}
@@ -38,100 +43,92 @@ Resize.prototype.initialize = function () {
 		throw(new Error("Invalid settings specified for the resizer."));
 	}
 }
-Resize.prototype.compileWidthResize = function () {
-	var widthOrigMulChannels = this.widthOriginal << 2;
-	var heightOrigMulChannels = this.heightOriginal << 2;
-	var widthTargetMulChannels = this.targetWidth << 2;
-	var toCompile = "\
-	var ratioWeight = " + (this.widthOriginal / this.targetWidth) + ";\
-	var weight = 0;\
-	var actualPosition = 0;\
-	var amountToNext = 0;\
-	var outputBuffer = this.widthBuffer;\
-	var outputOffset = 0;\
-	var currentPosition = 0;\
-	var output = this.outputWidthWorkBench;\
-	var line = 0;\
-	var pixelOffset = 0;\
-	do {\
-		weight = ratioWeight;\
-		for (line = 0; line < " + heightOrigMulChannels + "; ++line) {\
-			output[line] = 0;\
-		}\
-		while (weight > 0 && actualPosition < " + widthOrigMulChannels + ") {\
-			amountToNext = 1 + actualPosition - currentPosition;\
-			if (weight >= amountToNext) {\
-				for (pixelOffset = line = 0; line < " + heightOrigMulChannels + "; pixelOffset += " + widthOrigMulChannels + ") {\
-					output[line++] += buffer[actualPosition + pixelOffset] * amountToNext;\
-					output[line++] += buffer[actualPosition + pixelOffset + 1] * amountToNext;\
-					output[line++] += buffer[actualPosition + pixelOffset + 2] * amountToNext;\
-					output[line++] += buffer[actualPosition + pixelOffset + 3] * amountToNext;\
-				}\
-				currentPosition = actualPosition = actualPosition + 4;\
-				weight -= amountToNext;\
-			}\
-			else {\
-				for (pixelOffset = line = 0; line < " + heightOrigMulChannels + "; pixelOffset += " + widthOrigMulChannels + ") {\
-					output[line++] += buffer[actualPosition + pixelOffset] * weight;\
-					output[line++] += buffer[actualPosition + pixelOffset + 1] * weight;\
-					output[line++] += buffer[actualPosition + pixelOffset + 2] * weight;\
-					output[line++] += buffer[actualPosition + pixelOffset + 3] * weight;\
-				}\
-				currentPosition += weight;\
-				break;\
-			}\
-		}\
-		for (pixelOffset = line = 0; line < " + heightOrigMulChannels + "; pixelOffset += " + widthTargetMulChannels + ") {\
-			outputBuffer[outputOffset + pixelOffset] = output[line++] / ratioWeight;\
-			outputBuffer[outputOffset + pixelOffset + 1] = output[line++] / ratioWeight;\
-			outputBuffer[outputOffset + pixelOffset + 2] = output[line++] / ratioWeight;\
-			outputBuffer[outputOffset + pixelOffset + 3] = output[line++] / ratioWeight;\
-		}\
-		outputOffset += 4;\
-	} while (outputOffset < " + widthTargetMulChannels + ");\
-	return outputBuffer;";
-	this.resizeWidth = Function("buffer", toCompile);
+Resize.prototype.resizeWidth = function (buffer) {
+	var ratioWeight = this.ratioWeightWidthPass;
+	var weight = 0;
+	var actualPosition = 0;
+	var amountToNext = 0;
+	var outputBuffer = this.widthBuffer;
+	var outputOffset = 0;
+	var currentPosition = 0;
+	var output = this.outputWidthWorkBench;
+	var line = 0;
+	var pixelOffset = 0;
+	do {
+		weight = ratioWeight;
+		for (line = 0; line < this.originalHeightMultipliedByChannels; ++line) {
+			output[line] = 0;
+		}
+		while (weight > 0 && actualPosition < this.originalWidthMultipliedByChannels) {
+			amountToNext = 1 + actualPosition - currentPosition;
+			if (weight >= amountToNext) {
+				for (pixelOffset = line = 0; line < this.originalHeightMultipliedByChannels; pixelOffset += this.originalWidthMultipliedByChannels) {
+					output[line++] += buffer[actualPosition + pixelOffset] * amountToNext;
+					output[line++] += buffer[actualPosition + pixelOffset + 1] * amountToNext;
+					output[line++] += buffer[actualPosition + pixelOffset + 2] * amountToNext;
+					output[line++] += buffer[actualPosition + pixelOffset + 3] * amountToNext;
+				}
+				currentPosition = actualPosition = actualPosition + 4;
+				weight -= amountToNext;
+			}
+			else {
+				for (pixelOffset = line = 0; line < this.originalHeightMultipliedByChannels; pixelOffset += this.originalWidthMultipliedByChannels) {
+					output[line++] += buffer[actualPosition + pixelOffset] * weight;
+					output[line++] += buffer[actualPosition + pixelOffset + 1] * weight;
+					output[line++] += buffer[actualPosition + pixelOffset + 2] * weight;
+					output[line++] += buffer[actualPosition + pixelOffset + 3] * weight;
+				}
+				currentPosition += weight;
+				break;
+			}
+		}
+		for (pixelOffset = line = 0; line < this.originalHeightMultipliedByChannels; pixelOffset += this.targetWidthMultipliedByChannels) {
+			outputBuffer[outputOffset + pixelOffset] = output[line++] / ratioWeight;
+			outputBuffer[outputOffset + pixelOffset + 1] = output[line++] / ratioWeight;
+			outputBuffer[outputOffset + pixelOffset + 2] = output[line++] / ratioWeight;
+			outputBuffer[outputOffset + pixelOffset + 3] = output[line++] / ratioWeight;
+		}
+		outputOffset += 4;
+	} while (outputOffset < this.targetWidthMultipliedByChannels);
+	return outputBuffer;
 }
-Resize.prototype.compileHeightResize = function () {
-	var targetWidthMul = this.targetWidth << 2;
-	var toCompile = "\
-	var ratioWeight = " + (this.heightOriginal / this.targetHeight) + ";\
-	var weight = 0;\
-	var actualPosition = 0;\
-	var amountToNext = 0;\
-	var outputOffset = 0;\
-	var currentPosition = 0;\
-	var pixelOffset = 0;\
-	var output = this.outputHeightWorkBench;\
-	var outputBuffer = this.heightBuffer;\
-	do {\
-		weight = ratioWeight;\
-		for (pixelOffset = 0; pixelOffset < " + targetWidthMul + "; ++pixelOffset) {\
-			output[pixelOffset] = 0;\
-		}\
-		while (weight > 0 && actualPosition < " + (targetWidthMul * this.heightOriginal) + ") {\
-			amountToNext = 1 + actualPosition - currentPosition;\
-			if (weight >= amountToNext) {\
-				for (pixelOffset = 0; pixelOffset < " + targetWidthMul + "; ++pixelOffset) {\
-					output[pixelOffset] += buffer[actualPosition++] * amountToNext;\
-				}\
-				currentPosition = actualPosition;\
-				weight -= amountToNext;\
-			}\
-			else {\
-				for (pixelOffset = 0; pixelOffset < " + targetWidthMul + "; ++pixelOffset) {\
-					output[pixelOffset] += buffer[actualPosition + pixelOffset] * weight;\
-				}\
-				currentPosition += weight;\
-				break;\
-			}\
-		}\
-		for (pixelOffset = 0; pixelOffset < " + targetWidthMul + "; ++pixelOffset) {\
-			outputBuffer[outputOffset++] = output[pixelOffset] / ratioWeight;\
-		}\
-	} while (outputOffset < " + (targetWidthMul * this.targetHeight) + ");\
-	return outputBuffer;";
-	this.resizeHeight = Function("buffer", toCompile);
+Resize.prototype.resizeHeight = function (buffer) {
+	var ratioWeight = this.ratioWeightHeightPass;
+	var weight = 0;
+	var actualPosition = 0;
+	var amountToNext = 0;
+	var outputOffset = 0;
+	var currentPosition = 0;
+	var pixelOffset = 0;
+	var output = this.outputHeightWorkBench;
+	var outputBuffer = this.heightBuffer;
+	do {
+		weight = ratioWeight;
+		for (pixelOffset = 0; pixelOffset < this.targetWidthMultipliedByChannels; ++pixelOffset) {
+			output[pixelOffset] = 0;
+		}
+		while (weight > 0 && actualPosition < this.widthPassResultSize) {
+			amountToNext = 1 + actualPosition - currentPosition;
+			if (weight >= amountToNext) {
+				for (pixelOffset = 0; pixelOffset < this.targetWidthMultipliedByChannels; ++pixelOffset) {
+					output[pixelOffset] += buffer[actualPosition++] * amountToNext;
+				}
+				currentPosition = actualPosition;
+				weight -= amountToNext;
+			}
+			else {
+				for (pixelOffset = 0; pixelOffset < this.targetWidthMultipliedByChannels; ++pixelOffset) {
+					output[pixelOffset] += buffer[actualPosition + pixelOffset] * weight;
+				}
+				currentPosition += weight;
+				break;
+			}
+		}
+		for (pixelOffset = 0; pixelOffset < this.targetWidthMultipliedByChannels; ++pixelOffset) {
+			outputBuffer[outputOffset++] = output[pixelOffset] / ratioWeight;
+		}
+	} while (outputOffset < this.finalResultSize);
+	return outputBuffer;
 }
 Resize.prototype.resizer = function (buffer) {
 	return this.resizeHeight(this.resizeWidth(buffer));
@@ -142,13 +139,13 @@ Resize.prototype.bypassResampler = function (buffer) {
 }
 Resize.prototype.initializeFirstPassBuffers = function () {
 	//Initialize the internal width pass buffers:
-	this.widthBuffer = this.generateBuffer(this.targetWidth * this.heightOriginal * 4);
-	this.outputWidthWorkBench = this.generateBuffer(this.heightOriginal << 2);
+	this.widthBuffer = this.generateBuffer(this.widthPassResultSize);
+	this.outputWidthWorkBench = this.generateBuffer(this.originalHeightMultipliedByChannels);
 }
 Resize.prototype.initializeSecondPassBuffers = function () {
 	//Initialize the internal height pass buffers:
-	this.heightBuffer = this.generateBuffer(this.targetWidth * this.targetHeight * 4);
-	this.outputHeightWorkBench = this.generateBuffer(this.targetWidth << 2);
+	this.heightBuffer = this.generateBuffer(this.finalResultSize);
+	this.outputHeightWorkBench = this.generateBuffer(this.targetWidthMultipliedByChannels);
 }
 Resize.prototype.generateBuffer = function (bufferLength) {
 	//Generate a typed array buffer:
