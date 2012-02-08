8644e7678f081735b2c136cfd825eb42fe0af9a7
Grant Galitz <grantgalitz@gmail.com>
2012-02-08 02:22:12 -0500

    Software gfx resizer update.

--- js/other/resize.js
+++ js/other/resize.js
@@ -52,17 +52,13 @@ Resize.prototype.compileWidthResize = function () {
 	var amountToNext = 0;\
 	var outputBuffer = this.widthBuffer;\
 	var outputOffset = 0;\
-	var currentPosition = 0;";
-	for (var line = 0; line < heightOrigMulChannels; ++line) {
-		toCompile += "\
-	var output" + line + " = 0;";
-	}
-	toCompile += "\
+	var currentPosition = 0;\
+	var output = this.outputWidthWorkBench;\
 	do {\
 		weight = ratioWeight;";
 	for (line = 0; line < heightOrigMulChannels; ++line) {
 		toCompile += "\
-		output" + line + " = 0;";
+		output[" + line + "] = 0;";
 	}
 	toCompile += "\
 		while (weight > 0 && actualPosition < " + widthOrigMulChannels + ") {\
@@ -71,12 +67,12 @@ Resize.prototype.compileWidthResize = function () {
 	for (pixelOffset = line = 0; line < heightOrigMulChannels; pixelOffset += widthOrigMulChannels) {
 		temp_var = pixelOffset;
 		toCompile += "\
-				output" + (line++) + " += buffer[actualPosition" + ((temp_var > 0) ? (" + " + temp_var) : "") + "] * amountToNext;";
+				output[" + (line++) + "] += buffer[actualPosition" + ((temp_var > 0) ? (" + " + temp_var) : "") + "] * amountToNext;";
 		++temp_var;		
 		toCompile += "\
-				output" + (line++) + " += buffer[actualPosition + " + (temp_var++) + "] * amountToNext;\
-				output" + (line++) + " += buffer[actualPosition + " + (temp_var++) + "] * amountToNext;\
-				output" + (line++) + " += buffer[actualPosition + " + temp_var + "] * amountToNext;";
+				output[" + (line++) + "] += buffer[actualPosition + " + (temp_var++) + "] * amountToNext;\
+				output[" + (line++) + "] += buffer[actualPosition + " + (temp_var++) + "] * amountToNext;\
+				output[" + (line++) + "] += buffer[actualPosition + " + temp_var + "] * amountToNext;";
 	}
 	toCompile += "\
 				currentPosition = actualPosition = actualPosition + 4;\
@@ -86,12 +82,12 @@ Resize.prototype.compileWidthResize = function () {
 	for (pixelOffset = line = 0; line < heightOrigMulChannels; pixelOffset += widthOrigMulChannels) {
 		temp_var = pixelOffset;
 		toCompile += "\
-				output" + (line++) + " += buffer[actualPosition" + ((temp_var > 0) ? (" + " + temp_var) : "") + "] * weight;";
+				output[" + (line++) + "] += buffer[actualPosition" + ((temp_var > 0) ? (" + " + temp_var) : "") + "] * weight;";
 		++temp_var;		
 		toCompile += "\
-				output" + (line++) + " += buffer[actualPosition + " + (temp_var++) + "] * weight;\
-				output" + (line++) + " += buffer[actualPosition + " + (temp_var++) + "] * weight;\
-				output" + (line++) + " += buffer[actualPosition + " + temp_var + "] * weight;";
+				output[" + (line++) + "] += buffer[actualPosition + " + (temp_var++) + "] * weight;\
+				output[" + (line++) + "] += buffer[actualPosition + " + (temp_var++) + "] * weight;\
+				output[" + (line++) + "] += buffer[actualPosition + " + temp_var + "] * weight;";
 	}
 	toCompile += "\
 				currentPosition += weight;\
@@ -101,12 +97,12 @@ Resize.prototype.compileWidthResize = function () {
 	for (pixelOffset = line = 0; line < heightOrigMulChannels; pixelOffset += widthTargetMulChannels) {
 		temp_var = pixelOffset;
 		toCompile += "\
-		outputBuffer[outputOffset" + ((temp_var > 0) ? (" + " + temp_var) : "") + "] = output" + (line++) + " / ratioWeight;";
+		outputBuffer[outputOffset" + ((temp_var > 0) ? (" + " + temp_var) : "") + "] = output[" + (line++) + "] / ratioWeight;";
 		++temp_var;
 		toCompile += "\
-		outputBuffer[outputOffset + " + (temp_var++) + "] = output" + (line++) + " / ratioWeight;\
-		outputBuffer[outputOffset + " + (temp_var++) + "] = output" + (line++) + " / ratioWeight;\
-		outputBuffer[outputOffset + " + temp_var + "] = output" + (line++) + " / ratioWeight;";
+		outputBuffer[outputOffset + " + (temp_var++) + "] = output[" + (line++) + "] / ratioWeight;\
+		outputBuffer[outputOffset + " + (temp_var++) + "] = output[" + (line++) + "] / ratioWeight;\
+		outputBuffer[outputOffset + " + temp_var + "] = output[" + (line++) + "] / ratioWeight;";
 	}
 	toCompile += "\
 		outputOffset += 4;\
@@ -126,17 +122,13 @@ Resize.prototype.compileHeightResize = function () {
 	var amountToNext = 0;\
 	var outputBuffer = this.heightBuffer;\
 	var outputOffset = 0;\
-	var currentPosition = 0;";
-	for (var pixelOffset = 0; pixelOffset < targetWidthMul; ++pixelOffset) {
-		toCompile += "\
-	var output" + pixelOffset + " = 0;";
-	}
-	toCompile += "\
+	var currentPosition = 0;\
+	var output = this.outputHeightWorkBench;\
 	do {\
 		weight = ratioWeight;";
 	for (pixelOffset = 0; pixelOffset < targetWidthMul; ++pixelOffset) {
 		toCompile += "\
-		output" + pixelOffset + " = 0;";
+		output[" + pixelOffset + "] = 0;";
 	}
 	toCompile += "\
 		while (weight > 0 && actualPosition < " + totalPrevChannels + ") {\
@@ -144,7 +136,7 @@ Resize.prototype.compileHeightResize = function () {
 			if (weight >= amountToNext) {";
 	for (pixelOffset = 0; pixelOffset < targetWidthMul; ++pixelOffset) {
 		toCompile += "\
-				output" + pixelOffset + " += buffer[actualPosition++] * amountToNext;";
+				output[" + pixelOffset + "] += buffer[actualPosition++] * amountToNext;";
 	}
 	toCompile += "\
 				currentPosition = actualPosition;\
@@ -153,7 +145,7 @@ Resize.prototype.compileHeightResize = function () {
 			else {";
 	for (pixelOffset = 0; pixelOffset < targetWidthMul; ++pixelOffset) {
 		toCompile += "\
-				output" + pixelOffset + " += buffer[actualPosition" + ((pixelOffset > 0) ? (" + " + pixelOffset) : "") + "] * weight;";
+				output[" + pixelOffset + "] += buffer[actualPosition" + ((pixelOffset > 0) ? (" + " + pixelOffset) : "") + "] * weight;";
 	}
 	toCompile += "\
 				currentPosition += weight;\
@@ -162,7 +154,7 @@ Resize.prototype.compileHeightResize = function () {
 		}";
 	for (pixelOffset = 0; pixelOffset < targetWidthMul; ++pixelOffset) {
 		toCompile += "\
-		outputBuffer[outputOffset++] = output" + pixelOffset + " / ratioWeight;";
+		outputBuffer[outputOffset++] = output[" + pixelOffset + "] / ratioWeight;";
 	}
 	toCompile += "\
 	} while (outputOffset < " + totalChannels + ");\
@@ -176,31 +168,27 @@ Resize.prototype.bypassResampler = function (buffer) {
 	//Just return the buffer passsed:
 	return buffer;
 }
-Resize.prototype.initializeFirstPassBuffer = function () {
-	//Initialize the internal width pass buffer:
-	try {
-		this.widthBuffer = new Float16Array(this.targetWidth * this.heightOriginal * 4);
-	}
-	catch (error) {
-		try {
-			this.widthBuffer = new Float32Array(this.targetWidth * this.heightOriginal * 4);
-		}
-		catch (error) {
-			this.widthBuffer = [];
-		}
-	}
+Resize.prototype.initializeFirstPassBuffers = function () {
+	//Initialize the internal width pass buffers:
+	this.heightBuffer = this.generateBuffer(this.targetWidth * this.heightOriginal * 4);
+	this.outputWidthWorkBench = this.generateBuffer(this.heightOriginal << 2);
 }
-Resize.prototype.initializeSecondPassBuffer = function () {
-	//Initialize the internal buffer:
+Resize.prototype.initializeSecondPassBuffers = function () {
+	//Initialize the internal height pass buffers:
+	this.heightBuffer = this.generateBuffer(this.targetWidth * this.targetHeight * 4);
+	this.outputHeightWorkBench = this.generateBuffer(this.targetWidth << 2);
+}
+Resize.prototype.generateBuffer = function (bufferLength) {
+	//Generate a typed array buffer:
 	try {
-		this.heightBuffer = new Float16Array(this.targetWidth * this.targetHeight * 4);
+		return new Float16Array(bufferLength);
 	}
 	catch (error) {
 		try {
-			this.heightBuffer = new Float32Array(this.targetWidth * this.targetHeight * 4);
+			return new Float32Array(bufferLength);
 		}
 		catch (error) {
-			this.heightBuffer = [];
+			return [];
 		}
 	}
 }
