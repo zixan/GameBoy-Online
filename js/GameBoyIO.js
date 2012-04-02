9f3795de20a9ac0a256ecba18c55d0e4e9e1e827
Grant Galitz <grantgalitz@gmail.com>
2012-04-02 04:07:06 -0400

    Adjusting some internal heuristics to work around bugs in Google Chrome.

--- js/GameBoyIO.js
+++ js/GameBoyIO.js
@@ -9,8 +9,8 @@ var settings = [						//Some settings.
 	true,								//Colorize GB mode?
 	false,								//Disallow typed arrays?
 	4,									//Interval for the emulator loop.
-	8,									//Audio buffer span amount over x interpreter iterations.
-	20,									//Audio buffer size.
+	10,									//Audio buffer span amount over x interpreter iterations.
+	25,									//Audio buffer size.
 	false,								//Override to allow for MBC1 instead of ROM only (compatibility for broken 3rd-party cartridges).
 	false,								//Override MBC RAM disabling and always allow reading and writing to the banks.
 	false,								//Vertical blank event availability.
